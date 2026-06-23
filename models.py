986fd8c1ae8d16d09cfb4b762776f48bf8a6cdcc
Adesola Odubiyi <odubiyiadesola12@gmail.com>
2026-06-23 15:03:06 +0000

    made progress on the data model

--- models.py
+++ models.py
@@ -1,1 +1,18 @@
 # db schema models or object models
+# database entity structure and the strict Pydantic model Gemini must adhere to when compiling data from Google Search
+
+import re
+from pydantic import BaseModel, Field
+
+# Pydantic Schema for Structured GenAI and Search Outputs
+class MovieSchema(BaseModel):
+  #we'll need title, year, genre, and imdb rating
+  title: str = Field(description="Official movie title (i.e. 'Interstellar)")
+  year: int = Field(description="Four digit release year (i.e. '2014)")
+  genre: str = Field(description="Genre separated by commas (i.e. 'Action, Sci-Fie)")
+  imdb_rating: float = Field(description="Current IMDB score out of ten")
+
+  @property
+  def sluf(self) -> str:
+    "Generates a reliable, determinsitic primary key ("title-releaseyear"")"
+    # note, currently trying to figure out how to get the regex for this
